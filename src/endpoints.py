63c4340a22b820f40c9a52923d3298868ddc98b3
jpt-02 <jhntrahanas@gmail.com>
2026-06-19 01:52:54 -0400

    started tester

--- src/endpoints.py
+++ src/endpoints.py
@@ -13,22 +13,32 @@ import threading
 class TruthEndpoint:
     '''
     An endpoint that ONLY reports time directly after an NTP sync.
-    Serves as ground truth for testing other endpoints.
+    Serves as ground truth for testing other endpoints. Intended to 
+    be run on a NTPUpdater object with a shorter interval than that 
+    which it is being compared to.
     '''
-    def __init__(self):
+    def __init__(self, push:function):
         '''
-        
+        push: function to be called upon NTP sync, intended to store 
+            ground truth time and call now() from other endpoints to 
+            compare
         '''
         self._lock = threading.Lock()
         self.offset_anchor = OffsetAnchor(0)
+        self.push = push
 
     def callback(self, offset_anchor):
         '''
         Callback for each sync.
-        Stores the offset_anchor object locally.
+        Stores the offset_anchor object locally. Calls push with the 
+        calculated current time.
         '''
         with self._lock:
             self.offset_anchor = offset_anchor
+
+        perf_delta = time.perf_counter_ns() - offset_anchor.perf_ref
+        truth = (offset_anchor.time_ref + perf_delta)*1e-9 + offset_anchor.offset
+        self.push(truth)
 
 
 class SimpleEndpoint:
@@ -39,6 +49,7 @@ class SimpleEndpoint:
     def __init__(self):
         self._lock = threading.Lock()
         self.offset_anchor = OffsetAnchor(0)
+        # add easy setup parameter that automatically makes NTPUpdater object here
 
     def callback(self, offset_anchor):
         '''
---
+++ src/endpointtester.py
@@ -0,0 +1,10 @@
+'''
+Used for testing the accuracy of endpoints and saving/plotting data
+'''
+
+class EndpointTester:
+    '''
+    
+    '''
+    def __init__(self):
+        pass
